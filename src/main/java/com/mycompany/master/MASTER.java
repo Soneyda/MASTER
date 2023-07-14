2410e3f92d4cd58b6af04a82e071a7e41f150fe6
Soneyda <139030272+Soneyda@users.noreply.github.com>
2023-07-13 20:07:59 -0600

    Update MASTER.java
    Agregar clase hija

--- src/main/java/com/mycompany/master/MASTER.java
+++ src/main/java/com/mycompany/master/MASTER.java
@@ -23,5 +23,22 @@ public class MASTER {
     public String obtenerInformacion() {
         return "Nombre: " + nombre + "\nEdad: " + edad;
     }
+
+    public class Deportista extends Personas {
+    private String deporte;
+    
+    public Deportista(String nombre, int edad, String deporte) {
+        super(nombre, edad);
+        this.deporte = deporte;
+    }
+    
+    public String getDeporte() {
+        return deporte;
+    }
+    
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+    
 }
 
